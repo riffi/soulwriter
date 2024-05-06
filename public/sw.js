06e371d5d7065d675969a713da14b2e4c5ecdd89
Belenko Vladimir <vbelenko@ntdev.ru>
2024-05-06 11:41:40 +0300

    [develop] new version of pwa

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-importScripts('sw-toolbox.js');
-
-toolbox.precache(["index.html","/assets/*"]);
-toolbox.router.get('/icon/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
