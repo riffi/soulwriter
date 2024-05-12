33c19224408df221b1e89f4e8bf24ac9b0378a48
Владимир Беленко <vbelenko@ntdev.ru>
2024-05-12 12:14:29 +0300

    [develop] add import export

--- src/entities/Db/model/Db.ts
+++ src/entities/Db/model/Db.ts
@@ -1,4 +1,4 @@
-import Dexie, { Table } from 'dexie'
+import Dexie, {Table, Version} from 'dexie'
 import {ICharacter, ICharacterDictAttribute, ICharacterGroup} from "../../Character";
 import {IWorld} from "../../World";
 import {IBook} from "../../Book";
@@ -12,19 +12,24 @@ export class DbAdapter extends Dexie {
     worlds!: Table<IWorld>
     books!: Table<IBook>
 
+    static currentDbSchema: { [tableName: string]: string | null } =  {
+        characters: '++id, groupId, name, description, sex, bookId',
+        characterGroups: '++id, title, bookId',
+        characterAttributeDict: '++id, title, bookId',
+        worlds: '++id, title, bookId',
+        books: '++id, title, description, author',
+    }
+
+    static currentVersion = 5
+
     constructor() {
         const DBDeleteRequest = window.indexedDB.deleteDatabase("writer");
 
         super('soulwriter');
-        this.version(5).stores({
-            characters: '++id, groupId, name, description, sex, bookId',
-            characterGroups: '++id, title, bookId',
-            characterAttributeDict: '++id, title, bookId',
-            worlds: '++id, title, bookId',
-            books: '++id, title, description, author',
-        })
+        this.version(DbAdapter.currentVersion).stores(DbAdapter.currentDbSchema)
 
     }
 }
+
 
 export const db = new DbAdapter();
--- src/pages/General/ui/General.tsx
+++ src/pages/General/ui/General.tsx
@@ -1,3 +1,44 @@
+import {importDB, exportDB, importInto, peakImportFile} from "dexie-export-import";
+import {Button, Space} from "antd-mobile";
+import {db, DbAdapter} from "../../../entities/Db/model/Db.ts";
+import React, {useRef} from "react";
+import Dexie from "dexie";
+import {useNavigate} from "react-router-dom";
+
 export const General = () => {
-    return <h1>General</h1>
+
+    const fileRef = useRef<HTMLInputElement>()
+    const navigate = useNavigate()
+
+    const exportBase = async () => {
+        //
+        // Export to Blob
+        //
+        const blob = await exportDB(db, {prettyJson: true});
+        console.log(blob)
+        const url = window.URL.createObjectURL(blob);
+        const tempLink = document.createElement('a');
+        tempLink.href = url;
+        tempLink.setAttribute('download', 'exported.json');
+        tempLink.click();
+    }
+
+    const importBase = async () => {
+        const file: File | undefined = fileRef.current?.files?.[0]
+        await db.delete()
+
+        const  db2 = new Dexie("soulwriter");
+        db2.version(DbAdapter.currentVersion).stores(DbAdapter.currentDbSchema)
+        await db2.import(file)
+        window.location.replace('/books')
+
+    }
+
+    return (
+        <Space direction={"vertical"}>
+            <Button onClick={exportBase}>Экспорт</Button>
+            <input ref={fileRef} type="file"/>
+            <Button onClick={importBase}>Импорт</Button>
+        </Space>
+    )
 }
