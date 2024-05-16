47fc608b5101771fea6976e77d1910f8d74d15a4
Belenko Vladimir <vbelenko@ntdev.ru>
2024-05-16 17:37:43 +0300

    [develop] добавлено базовое редактирование данных мира

--- src/pages/world/WorldCard/ui/WorldCard.tsx
+++ src/pages/world/WorldCard/ui/WorldCard.tsx
@@ -1,12 +1,17 @@
 import {useSearchParams} from "react-router-dom";
 import {WorldViewForm} from "../../../../widgets/WorldViewForm"
+import {useSelector} from "react-redux";
+import {RootState} from "../../../../store.ts";
+import {NeedSelectBook} from "../../../../features/NeedSelectBook";
 export const WorldCard = () => {
     const [searchParams] = useSearchParams();
     const worldId = Number(searchParams.get('id'))
+    const currentBook = useSelector((state: RootState) => state.bookContext.currentBook)
 
+    if (!currentBook ||!currentBook?.id) return <NeedSelectBook/>
     return (
         <>
-            <WorldViewForm worldId={worldId}/>
+            <WorldViewForm worldId={worldId} bookId={currentBook.id}/>
         </>
     )
 }
--- src/widgets/WorldViewForm/ui/WorldViewForm.tsx
+++ src/widgets/WorldViewForm/ui/WorldViewForm.tsx
@@ -1,7 +1,29 @@
 import {IWorldViewFormProps} from "../model/types.ts";
+import {useWorldViewForm} from "../model/useWorldViewForm.ts";
+import {List} from "antd-mobile";
+import {InlineEdit} from "../../../shared/ui/InlineEdit";
+import {InlineTextArea} from "../../../shared/ui/InlineTextArea/ui/InlineTextArea.tsx";
 
 export const WorldViewForm = (props: IWorldViewFormProps) => {
+    const {
+        world,
+        changeBaseAttributeValue
+    } = useWorldViewForm(props.worldId, props.bookId)
+
     return (
-        <h1>World view form {props.worldId}</h1>
+        <List style={{"--border-top": "none", "--border-bottom": "none", "--font-size": "14px"}}>
+            <List.Item title={"Название"} key={"name"}>
+                <InlineEdit
+                    value={world?.title}
+                    onChange={(val) => changeBaseAttributeValue("title", val, world)}
+                />
+            </List.Item>
+            <List.Item title={"Описание"} key={"description"}>
+                <InlineTextArea
+                    value={world?.description}
+                    onChange={(val) => changeBaseAttributeValue("description", val, world)}
+                />
+            </List.Item>
+        </List>
     )
 }
