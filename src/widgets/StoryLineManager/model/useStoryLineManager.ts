d349b75c2ce68f68bf5db2d451e10bad539ce260
Belenko Vladimir <vbelenko@ntdev.ru>
2024-05-30 18:01:37 +0300

    [develop] добавлено отображение персонажей в списке сюжетных линий

--- src/widgets/StoryLineManager/model/useStoryLineManager.ts
+++ src/widgets/StoryLineManager/model/useStoryLineManager.ts
@@ -3,11 +3,32 @@ import {db} from "@entities/Db/model/Db.ts";
 import {IStoryLine} from "@entities/StoryLine/models/types.ts";
 
 export const useStoryLineManager = (bookId: number) => {
-    const storyLines = useLiveQuery(() => db.storyLines
-        .where("bookId")
-        .equals(bookId)
-        .toArray(), [bookId]
-    )
+    const storyLines = useLiveQuery(async () =>{
+        const storyLines = await db.storyLines
+                .where("bookId")
+                .equals(bookId)
+                .toArray()
+
+        // Добавляем информацию по сценам
+        await Promise.all (storyLines?.map (async storyLine => {
+            const storyLineCharacters = await db.storyLineCharacters
+                .where("storyLineId")
+                .equals(storyLine.id!)
+                .toArray()
+
+            const storyLineCharactersIds = storyLineCharacters.map((c) => c.characterId)
+
+
+            storyLine.characters = await db.characters
+                .where("id")
+                .anyOf(storyLineCharactersIds)
+                .toArray()
+
+            return storyLine
+        }))
+
+        return storyLines
+    } , [bookId])
 
     const onSaveNewStoryLine = (title: string) => {
         const storyLine: IStoryLine = {
